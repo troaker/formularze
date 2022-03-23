269dc2bd0697a257dd6358a88cebb683ef349eba
Troaker <m.drozdz1905@gmail.com>
2022-03-23 16:07:08 +0100

    update

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* Biblioteka */
 let output = document.getElementById('tbody');
 
 form.addEventListener("submit", function (event) {
@@ -15,34 +16,88 @@ form.addEventListener("submit", function (event) {
   localStorage.setItem("booksData", JSON.stringify(booksData));
   console.log(localStorage.getItem("booksData"));
 
-  dispData();
+  displayData();
 
   document.getElementById("form").reset();
 });
 
-function dispData() {
+function displayData() {
   const booksData = JSON.parse(localStorage.getItem("booksData"));
   output = document.getElementById('tbody');
   let i = 0;
 
-  output.innerHTML += 
-  "<tr><td>" + booksData[0].tytul
-  + "</td><td>" + booksData[0].autor 
-  + "</td><td>" + booksData[0].priorytet 
-  + "</td><td>" + booksData[0].kategoria
-  + "</td></tr>";
-
   booksData.forEach(element => {
-    i++;
     output.innerHTML += 
     "<tr><td>" + booksData[i].tytul
     + "</td><td>" + booksData[i].autor 
     + "</td><td>" + booksData[i].priorytet 
     + "</td><td>" + booksData[i].kategoria
+    + "<button>Usuń</button>"
     + "</td></tr>";
+    i++;
   });
 
-  console.log(booksData);
+  }
+
+  function  sortPriorytet() {
+    
+  }
+
+  function deleteBook() {
+
+  }
+
+displayData();
+
+/* Slideshow */
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-dispData();
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("slides");
+  const dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+/* Clock */
+function currentTime() {
+  let date = new Date();
+  let hh = date.getHours();
+  let mm = date.getMinutes();
+
+  if (hh > 12) 
+  {
+    session = "PM";
+  }
+
+  hh = (hh < 10) ? "0" + hh : hh;
+  mm = (mm < 10) ? "0" + mm : mm;
+
+  let time = hh + ":" + mm
+  document.getElementById("clock").innerText = time;
+  
+  setTimeout(currentTime(), 1000);
+}
+
+currentTime();
